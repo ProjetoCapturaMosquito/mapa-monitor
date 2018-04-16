00a657ac6ec6323c0a61351f58f4dfd797b2ddfb
Rafael Rabetti <rafael.rabetti@gmail.com>
2018-04-16 00:12:40 -0300

    Adding a sidebar and creating markers dynamically.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MapContainer from './map';
-import Header from './navbar'
+import Header from './navbar';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <div className="map"><MapContainer /></div>
+          <div className="map"><MapContainer /></div>
       </div>
     );
   }
--- src/map.js
+++ src/map.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Sidebar, Tab } from 'react-leaflet-sidebarv2';
 import 'leaflet/dist/leaflet.css';
 import './map.css';
+import './sidebar.css';
 import L from 'leaflet';
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -12,29 +14,95 @@ L.Icon.Default.mergeOptions({
 });
 
 class MapContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapsed: true,
+            selected: '',
+            lat: -15.505,
+            lng: -47.09,
+            zoom: 13,
+            markers: [[-15.5051, -47.091]],
+            latMarker: 0,
+            lngMarker: 0,
+        };
+        this.handleChange1 = this.handleChange1.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    state = {
-        lat: -15.505,
-        lng: -47.09,
-        zoom: 13,
+    handleChange1(event) {
+        this.setState({ latMarker: event.target.value });
+    }
+    handleChange2(event) {
+        this.setState({ lngMarker: event.target.value });
+    }
+
+    handleSubmit(event) {
+        const {markers} = this.state;
+        console.log({markers});
+        console.log(this.state.latMarker);
+        let position = [this.state.latMarker, this.state.lngMarker];
+        console.log(position);
+        markers.push(position);
+        this.setState({markers});
+        event.preventDefault();
+    }
+
+    onClose() {
+        this.setState({
+            collapsed: true,
+            selected: '',
+        });
+    }
+    onOpen(id) {
+        this.setState({
+            collapsed: false,
+            selected: id,
+        })
     }
 
     render() {
         const position = [this.state.lat, this.state.lng]
         return (
-            <Map center={position} zoom={this.state.zoom}>
-                <TileLayer
-                    attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={position}>
-                    <Popup>
-                        <span>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </span>
-                    </Popup>
-                </Marker>
-            </Map>
+            <div>
+                <Sidebar id="sidebar" collapsed={this.state.collapsed} selected={this.state.selected}
+                    onOpen={this.onOpen.bind(this)} onClose={this.onClose.bind(this)}>
+                    <Tab id="home" header="Home" icon="fa fa-home">
+                        <p>No place like home!</p>
+                    </Tab>
+                    <Tab id="messages" header="Máquinas" icon="fa fa-plus-square" className="disabled">
+                        <p>Cadastre novas máquinas:</p>
+                        <form onSubmit={this.handleSubmit}>
+                            <label>
+                                Latitude:
+                            <input type="text" value={this.state.latMarker} onChange={this.handleChange1} />
+                            </label>
+                            <label>
+                                Longitude:
+                            <input type="text" value={this.state.lngMarker} onChange={this.handleChange2} />
+                            </label>
+                            <input type="submit" value="Submit" />
+                        </form>
+                    </Tab>
+                    <Tab id="settings" header="Settings" icon="fa fa-cog" anchor="bottom">
+                        <p>Settings dialogue.</p>
+                    </Tab>
+                </Sidebar>
+                <Map className="sidebar-map" center={position} zoom={this.state.zoom}>
+                    <TileLayer
+                        attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {this.state.markers.map((position, idx) =>
+                        <Marker key={`marker-${idx}`} position={position}>
+                            <Popup>
+                                <span>Máquina de mosquito {position}</span>
+                            </Popup>
+                        </Marker>
+                    )}
+                </Map>
+            </div>
         )
     }
 }
