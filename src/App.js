dc968376781cf998ee1117fb4f97274ca11bb8ea
Rafael Rabetti <rafael.rabetti@gmail.com>
2018-04-20 14:29:53 -0300

    Começando a estrutura da store para persistir os dados.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { persistor, store } from './store';
 import logo from './logo.svg';
 import './App.css';
 import MapContainer from './map';
@@ -8,10 +11,14 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-          <div className="map"><MapContainer /></div>
-      </div>
+      // <Provider store={store}>
+        // <PersistGate persistor={persistor} loading={null}>
+          <div className="App">
+            <Header />
+            <div className="map"><MapContainer /></div>
+          </div>
+        // </PersistGate>
+      // </Provider>
     );
   }
 }
